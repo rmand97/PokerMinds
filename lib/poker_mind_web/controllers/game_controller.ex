d53d67eef7aa55782ebbd4c1250d268a804ff817
Stine Gammelager <stinegammelager00@gmail.com>
2026-04-10 09:26:35 +0200

    draft map_tablestate to filter what information the player gets

--- lib/poker_mind_web/controllers/game_controller.ex
+++ lib/poker_mind_web/controllers/game_controller.ex
@@ -3,13 +3,23 @@ defmodule PokerMindWeb.GameController do
 
   alias PokerMind.Engine.Match.Coordinator
   alias PokerMind.Engine.Match.Game
+  alias PokerMind.Engine.TableState
+  alias PokerMind.Engine.TableState.PlayerState
 
   def next_games(conn, %{"player_id" => player_id, "suite_id" => suite_id}) do
     coordinator_id = Coordinator.id(suite_id)
-    {games, _count} = Coordinator.next_games(coordinator_id, player_id)
+
+    games =
+      coordinator_id
+      |> Coordinator.next_games(player_id)
 
     # TODO: Filter what information the player gets
-    json(conn, %{data: games})
+    mapped_games =
+      Enum.map(games, fn game ->
+        map_tablestate(game)
+      end)
+
+    json(conn, %{data: mapped_games})
   end
 
   def next_games(conn, _params) do
@@ -25,7 +35,8 @@ defmodule PokerMindWeb.GameController do
       }) do
     case Game.apply_action(game_id, action, player_id) do
       {:ok, state} ->
-        json(conn, %{data: state})
+        mapped_state = %{state | game: map_tablestate(state.game)}
+        json(conn, %{data: mapped_state})
 
       {:error, reason} ->
         conn
@@ -43,5 +54,15 @@ defmodule PokerMindWeb.GameController do
     conn
     |> put_status(:bad_request)
     |> json(%{error: "player_id, game_id and action are required"})
+  end
+
+  # TODO: This is a draft
+  defp map_playerstate(%PlayerState{} = player) do
+    %{player_id: player.player_id}
+  end
+
+  defp map_tablestate(%TableState{} = tablestate) do
+    players = Enum.map(tablestate.players, fn player -> map_playerstate(player) end)
+    %{id: tablestate.id, players: players}
   end
 end
