c683088cc78f9ee78f3032230985d1b6f19c4e59
Rolf Andersen <rolf.m.andersen@it-minds.dk>
2026-04-13 10:18:21 +0200

    fix: linelength of comment :)

--- lib/poker_mind/Engine/actions.ex
+++ lib/poker_mind/Engine/actions.ex
@@ -22,7 +22,8 @@ defmodule PokerMind.Engine.Actions do
     end
   end
 
-  # def apply_action(%TableState{} = state, %{type: :call, player_id: player_id, amount: amount}) when is_binary(player_id) do
+  # def apply_action(%TableState{} = state, %{type: :call, player_id: player_id, amount: amount})
+  #     when is_binary(player_id) do
   #   with :ok <- validate_turn(player_id) do
   #     state.phase
   #     |> deduct_chips(player_id, amount)
